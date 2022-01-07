edfd3bb80125fc1b2b8e20d95ce80e85df8aec26
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-01-08 01:40:42 +0900

    add-file

--- db/migrate/20220107164027_create_members.rb
+++ db/migrate/20220107164027_create_members.rb
@@ -2,7 +2,7 @@ class CreateMembers < ActiveRecord::Migration[6.1]
   def change
     create_table :members do |t|
       t.text :name
-      t.text :plan
+      t.time :plan
 
       t.timestamps
     end
