6d97b70a41b2bb282b35421fe17cbc4023d2dcff
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-01-07 14:25:49 +0900

    add-file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def top
   end
+
+  def create
+    @member = Member.new(name: params[:name], plan: params[:times])
+    @member.save
+    redirect_to('/')
+  end
 end
