8694cfe76f5b439cc24c2718ae51da51fcff6c33
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-01-10 16:13:02 +0900

    add-file

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_07_164027) do
+ActiveRecord::Schema.define(version: 2022_01_10_071045) do
 
   create_table "members", force: :cascade do |t|
     t.text "name"
-    t.time "plan"
+    t.datetime "plan"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
