db38442bd7f12ef9f88dd72ed9ff0d491a89f918
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-02-07 21:32:37 +0900

    予約機能を追加していく

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get '/' => 'home#top'
+  get '/res' => 'home#reservation'
+  get '/enter' => 'home#enter'
   post '/create' => 'home#create'
   get '/:id' => 'home#show'
+  get '/res' => 'home#reservation'
+
   post '/:id/destroy' => 'home#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
